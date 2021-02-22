5736634648dd2760235c8c3f512d41c545465d1a
Robert Schumacher <ras0219@outlook.com>
2021-02-22 04:03:55 +0000

    Disable go lint

--- server/server.go
+++ server/server.go
@@ -34,15 +34,15 @@ const (
 )
 
 var GameStateMappings = []NameValueMapping{
-	NameValueMapping{
+	{
 		Name:  "lobby",
 		Value: LOBBY,
 	},
-	NameValueMapping{
+	{
 		Name:  "playing",
 		Value: PLAYING,
 	},
-	NameValueMapping{
+	{
 		Name:  "finished",
 		Value: FINISHED,
 	},
@@ -57,23 +57,23 @@ const (
 )
 
 var GameRoleMappings = []NameValueMapping{
-	NameValueMapping{
+	{
 		Name:  "none",
 		Value: NONE,
 	},
-	NameValueMapping{
+	{
 		Name:  "retailer",
 		Value: RETAILER,
 	},
-	NameValueMapping{
+	{
 		Name:  "wholesaler",
 		Value: WHOLESALER,
 	},
-	NameValueMapping{
+	{
 		Name:  "distributer",
 		Value: DISTRIBUTER,
 	},
-	NameValueMapping{
+	{
 		Name:  "manufacturer",
 		Value: MANUFACTURER,
 	},
